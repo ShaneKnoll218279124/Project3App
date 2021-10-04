a0a29d500db42e8751dfcc24f65f8a8057f1d05e
Dylan Koevort <218088159@mycput.ac.za>
2021-10-04 18:38:12 +0200

    basic attempt at live location

--- app/src/main/java/za/ac/cput/project3safeapp/LiveLocationActivity.java
+++ app/src/main/java/za/ac/cput/project3safeapp/LiveLocationActivity.java
@@ -1,18 +1,57 @@
 package za.ac.cput.project3safeapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class LiveLocationActivity extends AppCompatActivity {
+
+    private final int REQUEST_LOCATION = 1;
+    private static final int REQUEST_SEND_SMS = 0;
+    private LocationManager locationManager;
+    private String latitude, longitude, message, phoneNumber;
+    private TextView showLocation;
+    private EditText txtMessage, txtPhoneNumber;
+    private Button sendBtn;
+
+    // TODO: NEED TO REFACTOR REQUESTING OF PERMISSIONS
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_location);
+
+        ActivityCompat.requestPermissions( this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+
+        txtMessage = findViewById(R.id.liveLocationMessage);
+        txtPhoneNumber = findViewById(R.id.liveLocationPhone);
+        sendBtn = findViewById(R.id.liveLocationSendBtn);
+        showLocation = findViewById(R.id.latLongTextView);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMSMessage();
+            }
+        });
 
         ImageButton backBtn = (ImageButton) findViewById(R.id.btnBack);
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -23,4 +62,74 @@ public class LiveLocationActivity extends AppCompatActivity {
             }
         });
     }
+
+    protected void sendSMSMessage() {
+        phoneNumber = txtPhoneNumber.getText().toString();
+        message = txtMessage.getText().toString();
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS}, REQUEST_SEND_SMS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        if (requestCode == REQUEST_SEND_SMS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationCheck();
+                message = message + "\nTHEIR LOCATION IS: " + "\n\n" + "Latitude: \n" + latitude + "\n" + "Longitude: \n" + longitude;
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNumber, null, message, null, null);
+                Toast.makeText(getApplicationContext(), "SMS sent.", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "SMS failed, please try again.", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    private void locationCheck() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            OnGPS();
+        } else {
+            getLocation();
+        }
+    }
+
+    private void OnGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(LiveLocationActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (locationGPS != null) {
+                double lat = locationGPS.getLatitude();
+                double longi = locationGPS.getLongitude();
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+
+                showLocation.setText("Your Location: " + "\n" + "Latitude: " + latitude + "\n" + "Longitude: " + longitude);
+            } else {
+                Toast.makeText(this, "Unable to find location.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
