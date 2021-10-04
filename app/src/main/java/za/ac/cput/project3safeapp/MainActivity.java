e0fded4fa06e2f9ea11b332f70779a22b9eb21b8
Jonathan <HitsLikeTruck@users.noreply.github.com>
2021-10-04 12:15:15 +0200

    Removed previous database testing code.

--- app/src/main/java/za/ac/cput/project3safeapp/MainActivity.java
+++ app/src/main/java/za/ac/cput/project3safeapp/MainActivity.java
@@ -1,38 +1,19 @@
 package za.ac.cput.project3safeapp;
 
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.media.session.PlaybackState;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-
-    MyDatabaseHelper myDB;
-    ArrayList<String> _id, Username, Surname, Number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Database ->
-        myDB = new MyDatabaseHelper(MainActivity.this);
-        _id = new ArrayList<>();
-        Username = new ArrayList<>();
-        Surname = new ArrayList<>();
-        Number = new ArrayList<>();
-
-        storedArrayData();
-        //End ->
 
         ImageButton settingsBtn = (ImageButton) findViewById(R.id.btnSetting);
         settingsBtn.setOnClickListener(new View.OnClickListener() {
@@ -78,21 +59,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    //Database
-    void storedArrayData(){
-        Cursor cursor = myDB.readData();
-        if(cursor.getCount() == 0){
-            Toast.makeText(this,"No data.", Toast.LENGTH_SHORT).show();
-        } else {
-            while(cursor.moveToNext()){
-                _id.add(cursor.getString(0));
-                Username.add(cursor.getString(1));
-                Surname.add(cursor.getString(2));
-                Number.add(cursor.getString(3));
-            }
-        }
     }
 
     @Override
