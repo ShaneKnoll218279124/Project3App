6b5752b55034bcad17fa4a5e076894e6c436a127
Dylan Koevort <218088159@mycput.ac.za>
2021-10-17 19:25:00 +0200

    Fixed EmergencyServicesActivity

--- app/src/main/java/za/ac/cput/project3safeapp/EmergencyServicesActivity.java
+++ app/src/main/java/za/ac/cput/project3safeapp/EmergencyServicesActivity.java
@@ -1,18 +1,25 @@
 package za.ac.cput.project3safeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class EmergencyServicesActivity extends AppCompatActivity {
     DatabaseHelper mDatabaseHelper;
     private String ambNum, polNum, fireNum, nwNum;
+    private static final int REQUEST_CALL = 1;
+    private String btnPressed = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,9 +51,8 @@ public class EmergencyServicesActivity extends AppCompatActivity {
         btnAmbulance.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:"+ ambNum));
-                startActivity(callIntent);
+                btnPressed = "Ambulance";
+                callAmbulance();
             }
         });
 
@@ -54,9 +60,8 @@ public class EmergencyServicesActivity extends AppCompatActivity {
         btnPolice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:"+ polNum));
-                startActivity(callIntent);
+                btnPressed = "Police";
+                callPolice();
             }
         });
 
@@ -64,9 +69,8 @@ public class EmergencyServicesActivity extends AppCompatActivity {
         btnFire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:"+ fireNum));
-                startActivity(callIntent);
+                btnPressed = "Fire";
+                callFire();
             }
         });
 
@@ -74,10 +78,81 @@ public class EmergencyServicesActivity extends AppCompatActivity {
         btnNeighbourWatch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:"+ nwNum));
-                startActivity(callIntent);
+                btnPressed = "Neighbourhood Watch";
+                callNeighbourhoodWatch();
             }
         });
+    }
+
+    public void callAmbulance() {
+        if (ActivityCompat.checkSelfPermission(EmergencyServicesActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(EmergencyServicesActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + ambNum));
+            startActivity(callIntent);
+        }
+    }
+
+    public void callPolice() {
+        if (ActivityCompat.checkSelfPermission(EmergencyServicesActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(EmergencyServicesActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + polNum));
+            startActivity(callIntent);
+        }
+    }
+
+    public void callFire() {
+        if (ActivityCompat.checkSelfPermission(EmergencyServicesActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(EmergencyServicesActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + fireNum));
+            startActivity(callIntent);
+        }
+    }
+
+    public void callNeighbourhoodWatch() {
+        if (ActivityCompat.checkSelfPermission(EmergencyServicesActivity.this,
+                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(EmergencyServicesActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:" + nwNum));
+            startActivity(callIntent);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                switch (btnPressed) {
+                    case "Ambulance":
+                        callAmbulance();
+                        break;
+                    case "Police":
+                        callPolice();
+                        break;
+                    case "Fire":
+                        callFire();
+                        break;
+                    case "Neighbourhood Watch":
+                        callNeighbourhoodWatch();
+                }
+            } else {
+                Toast.makeText(this, "PERMISSION DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
